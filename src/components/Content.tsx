6e9bbb79b63131a70b00b14a9762f9eb160969e6
jamlmao <jajamagustin0624@gmail.com>
2025-08-01 22:14:23 +0800

    fixed errors from vercel logs

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -11,14 +11,14 @@ const ContentSection = () => {
     <div className="mx-auto max-w-5xl px-6">
         <div className="grid gap-6 md:grid-cols-2 md:gap-12">
             <h2 className="text-4xl font-medium text-white">
-                 Rent N'Go brings together cars, convenience, and control.
+                 Rent N&apos;Go brings together cars, convenience, and control.
             </h2>
             <div className="space-y-6">
                 <p className="text-white/90">
-                 Rent N'Go isn’t just a rental service it’s your all-in-one mobility partner. From compact cars to SUVs, from daily bookings to long-term plans, we make it effortless to get on the road with speed and confidence.
+                 Rent N&apos;Go isn&apos;t just a rental service it&apos;s your all-in-one mobility partner. From compact cars to SUVs, from daily bookings to long-term plans, we make it effortless to get on the road with speed and confidence.
                 </p>
                 <p className="text-white/90">
-                <span className="font-bold text-white"> Rent N'Go is your go-to for all things mobility. </span> 
+                <span className="font-bold text-white"> Rent N&apos;Go is your go-to for all things mobility. </span> 
                  We offer a wide range of cars, from compact sedans topremium luxury vehicles and spacious SUVs, ensuring you find the perfect ride for your needs.
                 </p>
                 <Button
